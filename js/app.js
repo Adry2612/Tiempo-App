7a91cfdad1806ccb5604f21f19c3a08ee44d17b2
Adry2612 <adrianvidalopez12@gmail.com>
2020-05-28 12:28:43 +0200

    Insertar datos de la API a la aplicación y convesion de kelvin a celsius y fahrenheit

--- js/app.js
+++ js/app.js
@@ -24,8 +24,6 @@ $().ready(function(){
                     insertarDatos(response);
                 }
             });
-
-            
     });
 
     }else{
@@ -34,8 +32,73 @@ $().ready(function(){
 });
 
 function insertarDatos(datos){
-    var localizacion = datos.name; 
-    $('.localizacion').append(localizacion);
+    // Información de localización.
+    $('.localizacion').html(datos.name);
 
-    var temperatura = datos.main.temp;
+    //Información de temperatura. (Cambio de kelvin a grados.)
+    var temp = kelvinACelsius(datos.main.temp);
+    $('#temp').html(temp);
+    $('#temp').addClass('celsius');
+    
+    // Al hacer click sobre la temperatura podremos cambiar entre grados celsius o fahrenheit.
+    $('.seccionTemperatura').click(function(){
+        if($('#temp').hasClass('celsius')){
+            temp = kelvinAFahrenheit(datos.main.temp);
+            $('#temp').html(temp);
+            // Eliminamos la clase anterior y añadimos la nueva clase.
+            $('#temp').addClass('fahrenheit').removeClass('celsius');
+            
+            // Lo mismo realizamos con el span.
+            $('span').remove();
+            $('.seccionTemperatura').append($('<span>').html('º F'));
+        } else{
+            temp = kelvinACelsius(datos.main.temp);
+            $('#temp').html(temp);
+            // Eliminamos la clase anterior y añadimos la nueva clase.
+            $('#temp').removeClass('fahrenheit').addClass('celsius');
+
+            // Lo mismo realizamos con el span.
+            $('span').remove();
+            $('.seccionTemperatura').append($('<span>').html('º C'));
+        }
+    });
+
+    // Descripciones de temperatura.
+    if (temp < -10){
+        $('.descripcionTemperatura').text('Hace un frio que congela las piedras.');
+    } else if (temp < 0){
+        $('.descripcionTemperatura').text('Hace muchisimo frio.');
+    } else if (temp < 10){
+        $('.descripcionTemperatura').text('Hace frio.');
+    } else if (temp < 20){
+        $('.descripcionTemperatura').text('Hace un poquito de frio.');
+    } else if (temp < 30){
+        $('.descripcionTemperatura').text('La temperatura es agradable.');
+    } else{
+        $('.descripcionTemperatura').text('Hace mucho calor.');
+    }
+
+    // Agregar icono correspondiente.
+    var icono = `http://openweathermap.org/img/wn/${datos.weather[0].icon}@2x.png`;
+    var img = $('<img>').attr('src', icono);
+    var p = $('<p>');
+    
+    p.append(img);
+    $('.zonaHoraria').append(p);
+}
+
+function kelvinACelsius(kelvin){
+    // Formula kelvin a grados.
+    var grados = kelvin - 273.15;
+
+    // Redondeamos a un decimal.
+    return grados.toFixed(1);
+}
+
+function kelvinAFahrenheit(kelvin){
+    // Formula kelvin a fahrenheit.
+    var grados = (kelvin - 273.15) * 1.8 + 32;
+
+    // Redondeamos a un decimal.
+    return grados.toFixed(1);
 }
